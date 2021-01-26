365e9e21c25ec1f9695890ff021567f5ecd9be51
RickSandov <i.s.ricardo.sandoval@gmail.com>
2021-01-25 18:02:40 -0600

    Hero page changed

--- js/index.js
+++ js/index.js
@@ -19,3 +19,47 @@ function myFunction() {
 }
 
 window.onscroll = function() {myFunction()};
+
+
+
+
+const optionsChildren = document.getElementsByClassName('option');
+
+
+// optionsChildren[0].classList.add('optionActive');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let last = 0;
+
+const changeOption = async () => {
+  current = 0;
+  
+  optionsChildren[last].classList.remove('optionActive');
+  optionsChildren[current].classList.add('optionActive');
+  await sleep(3500);
+
+  optionsChildren[current].classList.remove('optionActive');
+  current++;
+  optionsChildren[current].classList.add('optionActive');
+  await sleep(3500);
+
+  optionsChildren[current].classList.remove('optionActive');
+  current++;
+  optionsChildren[current].classList.add('optionActive');
+
+  await sleep(3500);
+  last = current;
+  
+}
+
+changeOption();
+setInterval(changeOption, 10530);
+
+
+
+
+
+
